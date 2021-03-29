6b2a8682266620ddfc1f71f9ac948d4e91376b80
Sai Ajay <saiajay220@gmail.com>
2021-03-29 13:09:48 +0530

    authentication hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -17,7 +17,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -35,7 +34,7 @@ res.render("register");
 app.post("/register",function(req,res){
     const newuser= new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newuser.save(function(err){
         if(err){
@@ -48,7 +47,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
     User.findOne({email:username},function(err,founduser){
         if(err){
             console.log(err);
